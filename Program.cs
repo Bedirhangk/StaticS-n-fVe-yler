b8ebb7cb111717d5877703904ef7681047c971fc
Bedirhan Gokkaya <bedirhangk12@gmail.com>
2023-12-07 19:02:00 +0300

    Static Kavramı çalışıldı

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace StaticSınıfVeÜyler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Çalışan Sayısı: "+Calısan.ÇalışanSay1);
+        // Calısan c1 =new Calısan("Bedirhan","Gökkaya","software");
+        // Console.WriteLine(Calısan.ÇalışanSay1);
+
+        Console.WriteLine("İki Sayının toplamı: "+ Mat.top(42,68));
+        Console.Write("İki Sayının Çıkartması: "+ Mat.cık(166,66));
+
+    }
+}
+
+class Calısan // class içerisindeki statik olan alanlar calışanlar sınıfında ilk kez erişildiğinde bir kere çalışır tekrardan sınıfa erişilirse veya o sınıfdan bir nesne türetilirse sıfırdan tekrardan başlamaz kaluğı yerden devam eder.
+{
+    private static int ÇalışanSay;
+
+    public static int ÇalışanSay1 { get => ÇalışanSay; }
+
+    private string Isim;
+    private string Soyisim;
+    private string Depratman;
+
+    static Calısan() // Statik kurucuların erişim belirteci olmaz.
+    {
+        ÇalışanSay=0;
+
+    }
+
+    public Calısan(string ısim, string soyisim, string depratman)
+    {
+        this.Isim=ısim;
+        this.Soyisim=soyisim;
+        this.Depratman=depratman;
+        ÇalışanSay++;
+    }
+
+}
+
+static class Mat //statik sınıflarda sadece statik metotlar kullanılabilir.
+{
+    public static int top(int a, int b)
+    {
+        return a+b;
+    }
+
+    public static int cık(int a, int b)
+    {
+        return a-b;
+    }
+}
